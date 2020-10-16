5f42d0c9343f10dab1af05b669aa799b6344a47b
KroFighter <48390227+KroFighter@users.noreply.github.com>
2020-10-16 19:41:12 +0100

    initial commit

---
+++ Scripts/Player.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody2D
+{
+    [Export] public float moveSpeed = 2f;
+
+    public override void _Process(float delta)
+    {
+        
+
+        if(Input.IsActionPressed("MoveLeft"))
+        {
+            MoveAndCollide(
+                new Vector2(-moveSpeed, 0)
+            );
+        }
+        
+        if (Input.IsActionPressed("MoveRight"))
+        {
+            MoveAndCollide(
+                new Vector2(moveSpeed, 0)
+            );
+        }
+        
+        if (Input.IsActionPressed("MoveUp"))
+        {
+            MoveAndCollide(
+                new Vector2(0, -moveSpeed)
+            );
+        }
+        
+        if (Input.IsActionPressed("MoveDown"))
+        {
+            MoveAndCollide(
+                new Vector2(0, moveSpeed)
+            );
+        }
+        
+    }
+}
